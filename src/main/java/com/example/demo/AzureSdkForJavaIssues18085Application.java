f6b7f858d3486a26b341d5dd8631640328f2517a
Rujun Chen <rujche@microsoft.com>
2020-12-14 09:03:36 +0800

    Create project by spring initlizer.

---
+++ src/main/java/com/example/demo/AzureSdkForJavaIssues18085Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSdkForJavaIssues18085Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSdkForJavaIssues18085Application.class, args);
+	}
+
+}
